d461defba3bb3f4e48801ce68c335e3bd9a12d26
Carter Janse <73023683+cjanse@users.noreply.github.com>
2023-12-16 12:04:11 +0100

    trying something else

--- dogscatsandchickens-app/app/layout.tsx
+++ dogscatsandchickens-app/app/layout.tsx
@@ -1,6 +1,7 @@
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import favicon from '../public/favicon.ico'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -19,6 +20,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+      <link rel="shortcut icon" href={favicon.src} type="image/x-icon" />
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   )
