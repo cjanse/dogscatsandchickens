d10d6567649b76e7719fe4539db65a70767dccbc
Carter Janse <73023683+cjanse@users.noreply.github.com>
2024-10-09 01:44:08 +0200

    AIPlayer can handle multiple types

--- dogscatsandchickens-app/app/controller/AIPlayerController.ts
+++ dogscatsandchickens-app/app/controller/AIPlayerController.ts
@@ -1,31 +1,40 @@
-import {Creature} from "../models/creature";
-import {Upgrade} from "../models/upgrade";
-import {Action} from "../models/action";
-import {Card} from "../models/card";
-import {Player} from "../models/player";
-import {GameBoard} from "../models/gameboard"
-import {GameController} from "./gameController";
+import { Creature } from "../models/creature";
+import { Upgrade } from "../models/upgrade";
+import { Action } from "../models/action";
+import { Card } from "../models/card";
+import { Player } from "../models/player";
+import { GameBoard } from "../models/gameboard"
+import { GameController } from "./gameController";
 
 export class AIPlayerController {
 
     gameController: GameController;
     player: Player;
     gameBoard: GameBoard;
+    aiType: string;
 
-    constructor(gameController: GameController){
+    constructor(gameController: GameController, type: string = "basic") {
         this.gameController = gameController;
         this.gameBoard = this.gameController.gameBoard;
         this.player = this.gameController.gameBoard.players[1]
+        this.aiType = type;
     }
 
     /* This function is reponsible for all of the AI moves*/
     move(): void {
+        if (this.aiType == "basic") {
+            this.basicMove();
+        }
+    }
+
+    /* This function is reponsible for all of the basic AI moves*/
+    basicMove(): void {
         //Doesn't do anything if the game is already over
-        if (this.gameController.gameOver){
+        if (this.gameController.gameOver) {
             return;
         }
         //player puts a creature down for first turn
-        if (this.player.turnNumber == 0){
+        if (this.player.turnNumber == 0) {
             this.player.moves = 0;
             this.gameController.addCreatureToField(this.gameController.randomCardId("Creature"));
             this.gameController.endofTurn();
@@ -35,16 +44,16 @@ export class AIPlayerController {
             //checks to see if player has a creature card or an action card
             if (this.gameController.hasCreature() || this.gameController.hasAction()) {
                 //uses action or places creature on field
-                if (this.gameController.hasAction() && !this.gameController.hasCreature()){
-                    if (this.player.hand.some(function(value, index, array) {return (value.id == 302)}) && this.gameBoard.discard.some(function(value, index, array) {return (value instanceof Creature)})) {
+                if (this.gameController.hasAction() && !this.gameController.hasCreature()) {
+                    if (this.player.hand.some(function (value, index, array) { return (value.id == 302) }) && this.gameBoard.discard.some(function (value, index, array) { return (value instanceof Creature) })) {
                         this.gameController.useAction(302);
                         this.gameBoard.players[this.gameBoard.currentPlayer].moves -= 1;
                     }
-                    else if (this.player.hand.some(function(value, index, array) {return (value.id == 304)}) && this.gameBoard.players[0].hand.some(function(value, index, array) {return (value instanceof Creature)})) {
+                    else if (this.player.hand.some(function (value, index, array) { return (value.id == 304) }) && this.gameBoard.players[0].hand.some(function (value, index, array) { return (value instanceof Creature) })) {
                         this.gameController.useBirdArmy(304, "Creature");
                         this.gameBoard.players[this.gameBoard.currentPlayer].moves -= 1;
                     }
-                    else if (this.player.hand.some(function(value, index, array) {return (value.id == 305)}) && this.gameBoard.players[0].hand.some(function(value, index, array) {return (value instanceof Creature)})) {
+                    else if (this.player.hand.some(function (value, index, array) { return (value.id == 305) }) && this.gameBoard.players[0].hand.some(function (value, index, array) { return (value instanceof Creature) })) {
                         this.gameController.useBirdArmy(305, "Creature");
                         this.gameBoard.players[this.gameBoard.currentPlayer].moves -= 1;
                     }
@@ -53,8 +62,8 @@ export class AIPlayerController {
                     this.gameController.addCreatureToField(this.gameController.randomCardId("Creature"));
                     this.gameBoard.players[this.gameBoard.currentPlayer].moves -= 1;
                 }
-                if (this.player.field.length > 0){
-                    if (this.gameBoard.deck.length > 0) {this.gameController.drawCard();}
+                if (this.player.field.length > 0) {
+                    if (this.gameBoard.deck.length > 0) { this.gameController.drawCard(); }
                     if (this.player.moves > 0 && this.player.hand.length > 0) {
                         this.randomReinforce();
                     }
@@ -64,17 +73,17 @@ export class AIPlayerController {
         }
         //Execute regular turn
         else {
-            if (this.gameBoard.deck.length > 0) {this.gameController.drawCard();}
+            if (this.gameBoard.deck.length > 0) { this.gameController.drawCard(); }
             //Determines randomly if the turn is reinforce or attack
-            if (Math.round(Math.random()) ? true : false || this.player.hand.length == 0){
+            if (Math.round(Math.random()) ? true : false || this.player.hand.length == 0) {
                 this.randomAttack()
             }
             else {
                 this.randomReinforce();
-                if (this.player.hand.length > 0){this.randomReinforce();}
+                if (this.player.hand.length > 0) { this.randomReinforce(); }
             }
             //Gets rid of extra cards
-            while (this.player.hand.length > 5){
+            while (this.player.hand.length > 5) {
                 this.randomDiscard();
             }
             this.gameController.endofTurn();
@@ -89,7 +98,7 @@ export class AIPlayerController {
         let cardId = this.gameController.randomCardId()
         //Places creature on field
         if (cardId < 200) {
-            if (this.gameController.canMatch(cardId)){
+            if (this.gameController.canMatch(cardId)) {
                 this.gameController.matchCreature(cardId)
                 this.gameController.matchCreatureActivateAbility(cardId)
             }
@@ -98,9 +107,9 @@ export class AIPlayerController {
             }
         }
         //places upgrade on field
-        else if (cardId > 199 && cardId < 300){
-            if (this.gameController.canPlaceUpgrade()){
-                this.gameController.placeUpgradeOnCreature(cardId,this.gameController.randomCreatureForUpgrade())
+        else if (cardId > 199 && cardId < 300) {
+            if (this.gameController.canPlaceUpgrade()) {
+                this.gameController.placeUpgradeOnCreature(cardId, this.gameController.randomCreatureForUpgrade())
             }
             else {
                 this.gameBoard.players[this.gameBoard.currentPlayer].moves += 1
