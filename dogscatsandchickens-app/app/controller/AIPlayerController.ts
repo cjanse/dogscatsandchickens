6e3401b758fae2bd62b009cc044c9985715bc9fd
Carter Janse <73023683+cjanse@users.noreply.github.com>
2024-10-10 02:48:32 +0200

    setting up for algorithmic player

--- dogscatsandchickens-app/app/controller/AIPlayerController.ts
+++ dogscatsandchickens-app/app/controller/AIPlayerController.ts
@@ -13,7 +13,7 @@ export class AIPlayerController {
     gameBoard: GameBoard;
     aiType: string;
 
-    constructor(gameController: GameController, type: string = "basic") {
+    constructor(gameController: GameController, type: string = "algorithmic") {
         this.gameController = gameController;
         this.gameBoard = this.gameController.gameBoard;
         this.player = this.gameController.gameBoard.players[1]
@@ -30,6 +30,77 @@ export class AIPlayerController {
         if (this.aiType == "basic") {
             this.basicMove();
         }
+        else if (this.aiType == "algorithmic") {
+            this.algorithmicMove();
+        }
+    }
+
+    /* This function is responsiable for all of the algorithmic AI moves*/
+    //CURRENT BEHAVES EXACTLY LIKE BASIC
+    algorithmicMove(): void {
+        let analysis = this.algorithmicAnalyze();
+
+        //player puts a creature down for first turn
+        if (this.player.turnNumber == 0) {
+            this.player.moves = 0;
+            this.gameController.addCreatureToField(this.gameController.randomCardId("Creature"));
+            this.gameController.endofTurn();
+        }
+        //player is forced to play an action or creature if they have no creature in front of them
+        else if (!this.gameController.hasFieldCreature()) {
+            //checks to see if player has a creature card or an action card
+            if (this.gameController.hasCreature() || this.gameController.hasAction()) {
+                //uses action or places creature on field
+                if (this.gameController.hasAction() && !this.gameController.hasCreature()) {
+                    if (this.player.hand.some(function (value, index, array) { return (value.id == 302) }) && this.gameBoard.discard.some(function (value, index, array) { return (value instanceof Creature) })) {
+                        this.gameController.useAction(302);
+                        this.gameBoard.players[this.gameBoard.currentPlayer].moves -= 1;
+                    }
+                    else if (this.player.hand.some(function (value, index, array) { return (value.id == 304) }) && this.gameBoard.players[0].hand.some(function (value, index, array) { return (value instanceof Creature) })) {
+                        this.gameController.useBirdArmy(304, "Creature");
+                        this.gameBoard.players[this.gameBoard.currentPlayer].moves -= 1;
+                    }
+                    else if (this.player.hand.some(function (value, index, array) { return (value.id == 305) }) && this.gameBoard.players[0].hand.some(function (value, index, array) { return (value instanceof Creature) })) {
+                        this.gameController.useBirdArmy(305, "Creature");
+                        this.gameBoard.players[this.gameBoard.currentPlayer].moves -= 1;
+                    }
+                }
+                if (this.gameController.hasCreature()) {
+                    this.gameController.addCreatureToField(this.gameController.randomCardId("Creature"));
+                    this.gameBoard.players[this.gameBoard.currentPlayer].moves -= 1;
+                }
+                if (this.player.field.length > 0) {
+                    if (this.gameBoard.deck.length > 0) { this.gameController.drawCard(); }
+                    if (this.player.moves > 0 && this.player.hand.length > 0) {
+                        this.randomReinforce();
+                    }
+                }
+            }
+            this.gameController.endofTurn();
+        }
+        //Execute regular turn
+        else {
+            if (this.gameBoard.deck.length > 0) { this.gameController.drawCard(); }
+            //Determines randomly if the turn is reinforce or attack
+            if (Math.round(Math.random()) ? true : false || this.player.hand.length == 0) {
+                this.randomAttack()
+            }
+            else {
+                this.randomReinforce();
+                if (this.player.hand.length > 0) { this.randomReinforce(); }
+            }
+            //Gets rid of extra cards
+            while (this.player.hand.length > 5) {
+                this.randomDiscard();
+            }
+            this.gameController.endofTurn();
+        }
+    }
+
+    /* This function analyzes all cards for algorithm AI Players */
+    algorithmicAnalyze(): { field: number[], hand: number[] } {
+        //FUNCTION STUB
+        return { field: [], hand: [] }
     }
 
     /* This function is reponsible for all of the basic AI moves*/
